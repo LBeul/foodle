367ab32d121eb61c97b91531a566f5b104590480
Louis <37245841+LBeul@users.noreply.github.com>
2023-10-30 16:43:41 +0100

    feat(frontend): :zap: initiate vite+react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div style={{ fontSize: '5rem' }}>🍜</div>
+      <h1>Foodle</h1>
+      <p>Work In Progress...</p>
+    </>
+  );
+}
+
+export default App;
