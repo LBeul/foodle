b7c3cbee2c36200ca99d56faffb79f3442c735e2
Louis <37245841+LBeul@users.noreply.github.com>
2023-11-13 14:45:26 +0100

    feat(frontend): :sparkles: update styles and fetch restaurants from API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import './App.css';
+import RestaurantsList from './components/RestaurantsList';
 
 function App() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  const fetchRestaurants = async () => {
+    try {
+      const response = await fetch('http://localhost:3003/api/restaurants');
+      const restaurants = await response.json();
+      setRestaurants(restaurants);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
       <div style={{ fontSize: '5rem' }}>🍜</div>
       <h1>Foodle</h1>
       <p>Work In Progress...</p>
+      <button onClick={fetchRestaurants}>Fetch restaurants</button>
+      {restaurants.length ? (
+        <RestaurantsList restaurants={restaurants} />
+      ) : (
+        <div>Click to load restaurants</div>
+      )}
     </>
   );
 }
