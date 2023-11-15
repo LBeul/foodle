d1ae0223326c5d3cc00e07d23b1e1992bfd28795
Louis <37245841+LBeul@users.noreply.github.com>
2023-11-15 11:12:48 +0100

    refactor(frontend): :zap: load restaurants on initial render

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RestaurantsList from './components/RestaurantsList/RestaurantsList';
-import { Button, Container, Heading, Text, VStack } from '@chakra-ui/react';
+import { Heading, Spinner, VStack } from '@chakra-ui/react';
 
 function App() {
   const [restaurants, setRestaurants] = useState([]);
 
-  const fetchRestaurants = async () => {
-    try {
-      const response = await fetch('http://localhost:3003/api/restaurants');
-      const restaurants = await response.json();
-      setRestaurants(restaurants);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const response = await fetch('http://localhost:3003/api/restaurants');
+        const restaurants = await response.json();
+        setRestaurants(restaurants);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchRestaurants();
+  }, []);
 
   return (
     <main>
       <VStack w='80%' mx='auto'>
         <Heading my={4}>Foodle</Heading>
-        <Button
-          onClick={fetchRestaurants}
-          my={4}
-          variant='outline'
-          colorScheme='purple'
-        >
-          Fetch restaurants
-        </Button>
+
         {restaurants.length ? (
           <RestaurantsList restaurants={restaurants} />
         ) : (
-          <Text>Click to load restaurants</Text>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='purple.100'
+            color='purple.500'
+            size='xl'
+          />
         )}
       </VStack>
     </main>
