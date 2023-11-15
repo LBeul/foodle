a2d26b8f62f5275ecf39b157f96ea58d8e5682a7
Louis <37245841+LBeul@users.noreply.github.com>
2023-11-15 18:37:17 +0100

    feat(frontend): :lipstick: enable darkmode

---
+++ frontend/theme.ts
@@ -0,0 +1,15 @@
+import { baseTheme, extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({
+  config,
+  colors: {
+    primary: baseTheme.colors.purple,
+  },
+});
+
+export default theme;
