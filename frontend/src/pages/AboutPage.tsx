3c51217a965012b3debdca84ece18835cfcac31d
lbeul <l.beul@outlook.de>
2023-12-27 11:19:18 +0100

    chore(frontend): :clown_face: add mock content to about page

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,10 +1,33 @@
-import { Heading, VStack } from '@chakra-ui/react';
+import { Heading, Text, VStack } from '@chakra-ui/react';
 
 function AboutPage() {
   return (
     <main>
       <VStack w='80%' mx='auto'>
-        <Heading>Moin.</Heading>
+        <Heading>About Foodle</Heading>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+          architecto doloribus quos recusandae molestiae vero provident quaerat
+          suscipit. Id, ab ipsam non quidem autem dolor repudiandae magnam
+          officia asperiores adipisci! Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Molestiae ad totam animi ex ullam praesentium
+          quisquam quidem, omnis vitae saepe quas aliquid et suscipit iste
+          deserunt nisi? Necessitatibus, quia doloribus? Lorem ipsum, dolor sit
+          amet consectetur adipisicing elit. Nemo labore illum voluptas
+          doloremque dolor incidunt aut nobis numquam architecto soluta cumque
+          laudantium obcaecati aliquam, possimus aspernatur at? Cupiditate, cum
+          nobis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Laborum, architecto doloribus quos recusandae molestiae vero provident
+          quaerat suscipit. Id, ab ipsam non quidem autem dolor repudiandae
+          magnam officia asperiores adipisci! Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Molestiae ad totam animi ex ullam
+          praesentium quisquam quidem, omnis vitae saepe quas aliquid et
+          suscipit iste deserunt nisi? Necessitatibus, quia doloribus? Lorem
+          ipsum, dolor sit amet consectetur adipisicing elit. Nemo labore illum
+          voluptas doloremque dolor incidunt aut nobis numquam architecto soluta
+          cumque laudantium obcaecati aliquam, possimus aspernatur at?
+          Cupiditate, cum nobis?
+        </Text>
       </VStack>
     </main>
   );
