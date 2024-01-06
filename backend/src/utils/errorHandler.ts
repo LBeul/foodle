55adb3bc5ffb4a17fd9287bdcfc1086e9490b4b0
lbeul <l.beul@outlook.de>
2024-01-06 21:47:11 +0100

    fix(backend): :goal_net: catch JWT-related errors and return fitting error code

--- backend/src/utils/errorHandler.ts
+++ backend/src/utils/errorHandler.ts
@@ -15,6 +15,10 @@ const errorHandler = (
     return response.status(400).send({ error: 'invalid id' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
   }
   next(error);
 };
