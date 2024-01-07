1a57464a9caf54e8a215242d6cd113d8c8d47465
lbeul <l.beul@outlook.de>
2024-01-07 22:46:15 +0100

    refactor(frontend): :recycle: adjust variable naming and use global types

--- frontend/src/hooks/useRestaurants.ts
+++ frontend/src/hooks/useRestaurants.ts
@@ -1,24 +1,22 @@
-import { Restaurant } from '@/types';
+import { FetchState, Restaurant } from '@/types';
 import { useEffect, useState } from 'react';
-
-type FetchState = 'initial' | 'loading' | 'success' | 'error';
 
 const useRestaurants = () => {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-  const [state, setState] = useState<FetchState>('initial');
+  const [fetchState, setFetchState] = useState<FetchState>('initial');
   const [error, setError] = useState<Error>();
 
   const fetchRestaurants = () => {
-    setState('loading');
+    setFetchState('loading');
     fetch('http://localhost:3003/api/restaurants')
       .then((response) => response.json())
       .then((r) => {
         setRestaurants(r);
-        setState('success');
+        setFetchState('success');
       })
       .catch((e: Error) => {
         setError(e);
-        setState('error');
+        setFetchState('error');
       });
   };
 
@@ -30,7 +28,7 @@ const useRestaurants = () => {
     fetchRestaurants();
   };
 
-  return { restaurants, error, state, refresh };
+  return { restaurants, error, fetchState, refresh };
 };
 
 export default useRestaurants;
