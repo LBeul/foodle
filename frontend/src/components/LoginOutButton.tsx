a4bb6953e25f53286ecd041fdb84c4eb29d3cc13
lbeul <l.beul@outlook.de>
2024-01-20 23:43:07 +0100

    fix(frontend): :truck: navigate to root page on logout click

--- frontend/src/components/LoginOutButton.tsx
+++ frontend/src/components/LoginOutButton.tsx
@@ -2,7 +2,7 @@ import AuthAtom from '@/stores/authStore';
 import { Button } from '@chakra-ui/react';
 import { useAtom } from 'jotai';
 import { ReactElement } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 interface PropTypes {
   variant: 'desktop' | 'mobile';
@@ -10,12 +10,15 @@ interface PropTypes {
 
 const LogInOutButton = ({ variant }: PropTypes): ReactElement => {
   const [authStatus, setAuthStatus] = useAtom(AuthAtom);
+  const navigate = useNavigate();
   const isDesktop = variant === 'desktop';
+
   return authStatus.isLoggedIn ? (
     <Button
       display={isDesktop ? { base: 'none', md: 'flex' } : undefined}
       onClick={() => {
         setAuthStatus({ isLoggedIn: false });
+        navigate('/');
       }}
     >
       Logout
